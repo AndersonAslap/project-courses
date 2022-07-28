4aa103b4fd440cd43a19cc257f21846b59df7ffd
Anderson Adolfo <anderson.adolfo1998@gmail.com>
2022-07-28 00:18:17 -0300

    feat:aplicaçao dos metodos delete, put e patch

--- src/modules/courses/courses.controller.ts
+++ src/modules/courses/courses.controller.ts
@@ -1,15 +1,28 @@
 import {
   Body,
   Controller,
+  Delete,
   Get,
   HttpCode,
   HttpStatus,
   Param,
+  Patch,
   Post,
+  Put,
   Res,
 } from '@nestjs/common';
 
-const courses = [];
+type CourseType = {
+  id: string;
+  name: string;
+  description: string;
+  price: number;
+  active: boolean;
+  created_at: Date;
+  updated_at: Date;
+};
+
+let courses: CourseType[] = [];
 
 @Controller('courses')
 export class CoursesController {
@@ -26,7 +39,51 @@ export class CoursesController {
   @Post()
   @HttpCode(HttpStatus.NO_CONTENT)
   create(@Body() body) {
-    const course = { id: String(courses.length + 1), ...body };
+    const course = {
+      id: String(courses.length + 1),
+      active: true,
+      ...body,
+      created_at: new Date(),
+      updated_at: new Date(),
+    } as CourseType;
     courses.push(course);
+  }
+
+  @Put(':id')
+  update(@Param('id') id: string, @Body() body, @Res() response) {
+    const course = courses.find((course) => course.id === id);
+    const index = courses.indexOf(course);
+    const courseUpdated = {
+      id,
+      ...body,
+      created_at: course.created_at,
+      updated_at: new Date(),
+    };
+    courses[index] = courseUpdated;
+    return response.status(200).json(courseUpdated);
+  }
+
+  @Patch('active/:id')
+  @HttpCode(HttpStatus.NO_CONTENT)
+  active(@Param('id') id: string) {
+    const course = courses.find((course) => course.id === id);
+    const index = courses.indexOf(course);
+
+    courses[index].active = true;
+  }
+
+  @Patch('desactive/:id')
+  @HttpCode(HttpStatus.NO_CONTENT)
+  desactive(@Param('id') id: string) {
+    const course = courses.find((course) => course.id === id);
+    const index = courses.indexOf(course);
+
+    courses[index].active = false;
+  }
+
+  @Delete(':id')
+  @HttpCode(HttpStatus.NO_CONTENT)
+  delete(@Param('id') id: string) {
+    courses = courses.filter((course) => course.id !== id);
   }
 }
