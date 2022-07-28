731e9b9af2bf59150e494db6235d546ec2ab293e
Anderson Adolfo <anderson.adolfo1998@gmail.com>
2022-07-28 17:17:08 -0300

    chore: adicionando a biblioteca @nestjs/mapped-types

--- src/modules/courses/dto/update-course.dto.ts
+++ src/modules/courses/dto/update-course.dto.ts
@@ -1,11 +1,23 @@
+import { IsNumber, IsString } from 'class-validator';
+
 export class UpdateCourseDto {
+  @IsString()
   readonly name: string;
+
+  @IsString()
   readonly description: string;
+
+  @IsNumber()
   readonly price: number;
 }
 
 export class UpdateCourseFildsDto {
+  @IsString()
   readonly name?: string;
+
+  @IsString()
   readonly description?: string;
+
+  @IsNumber()
   readonly price?: number;
 }
